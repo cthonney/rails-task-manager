cad65daeee65090d220dea8b4610c90fa745004c
cedric thonney <c.thonney@gmail.com>
2021-09-25 15:40:59 +0400

    no css

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # READ ALL TASKS
+  get "tasks", to: "tasks#index"
+
+
+
+  #CREATE TASK
+  post "tasks", to: "tasks#create"
+  get "tasks/new", to: "tasks#new", as: "task_new"
+
+  # READ ONE TASK
+  get "tasks/:id", to: "tasks#show", as: "task"
+
+  # UPDATE TASK
+  get "tasks/:id/edit", to: "tasks#edit", as: "task_edit"
+  patch "tasks/:id", to: "tasks#update"
+
+  # DELETE A TASK
+  delete 'tasks/:id', to: 'tasks#destroy'
+
 end
